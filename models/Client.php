19ad34b67bed1024844441a3d8b03ec5fbb45cb3
mpaymetov <muxa147@gmail.com>
2018-07-26 18:06:10 +0300

    add model client

---
+++ models/Client.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "client".
+ *
+ * @property string $id_client
+ * @property string $name
+ * @property string $created
+ * @property string $comment
+ * @property string $id_manager
+ *
+ * @property Manager $manager
+ * @property Project[] $projects
+ */
+class Client extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'client';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['name', 'comment', 'id_manager'], 'required'],
+            [['created'], 'safe'],
+            [['comment'], 'string'],
+            [['id_manager'], 'integer'],
+            [['name'], 'string', 'max' => 255],
+            [['id_manager'], 'exist', 'skipOnError' => true, 'targetClass' => Manager::className(), 'targetAttribute' => ['id_manager' => 'id_manager']],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id_client' => 'Id Client',
+            'name' => 'Name',
+            'created' => 'Created',
+            'comment' => 'Comment',
+            'id_manager' => 'Id Manager',
+        ];
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getManager()
+    {
+        return $this->hasOne(Manager::className(), ['id_manager' => 'id_manager']);
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getProjects()
+    {
+        return $this->hasMany(Project::className(), ['id_client' => 'id_client']);
+    }
+}
---
+++ views/client/_form.php
@@ -0,0 +1,29 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/* @var $this yii\web\View */
+/* @var $model app\models\Client */
+/* @var $form yii\widgets\ActiveForm */
+?>
+
+<div class="client-form">
+
+    <?php $form = ActiveForm::begin(); ?>
+
+    <?= $form->field($model, 'name')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'created')->textInput() ?>
+
+    <?= $form->field($model, 'comment')->textarea(['rows' => 6]) ?>
+
+    <?= $form->field($model, 'id_manager')->textInput(['maxlength' => true]) ?>
+
+    <div class="form-group">
+        <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
