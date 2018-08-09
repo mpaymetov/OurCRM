3065b24c8991c03c340c78fc32083ee7802e3d00
mpaymetov <muxa147@gmail.com>
2018-08-09 18:40:35 +0300

    edit manager view more link

--- views/site/_post_manager.php
+++ views/site/_post_manager.php
@@ -10,6 +10,6 @@ use yii\helpers\Url;
     <div class="panel-body">
         <p class="post_number">Manager number: <?= HtmlPurifier::process($model->id_manager) ?></p>
         <h4><?= Html::encode($model->name) ?></h4>
-        <a href=<?= Url::toRoute('manager/more') ?> class="btn btn_more">view more</a>
+        <a href=<?= Url::toRoute(['manager/view', 'id' => $model->id_manager]) ?> class="btn btn_more">view more</a>
     </div>
 </div>
