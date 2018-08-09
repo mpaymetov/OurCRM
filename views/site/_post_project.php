5d1f264ba5b91f18f25a6da467a6a518f10f164b
mpaymetov <muxa147@gmail.com>
2018-08-09 18:43:56 +0300

    edit project view more link

--- views/site/_post_project.php
+++ views/site/_post_project.php
@@ -2,12 +2,14 @@
 
 use yii\helpers\Html;
 use yii\helpers\HtmlPurifier;
+use yii\helpers\Url;
+
 ?>
 <div class="post panel">
     <div class="panel-body">
         <h2><?= Html::encode($model->name) ?></h2>
-        <?= HtmlPurifier::process($model->id_client) ?>
-        <a href="#" class="btn btn_more">view more</a>
+        <?= HtmlPurifier::process($model->id_project) ?>
+        <a href='<?= Url::to(['project/view', 'id' => $model->id_project]) ?>' class="btn btn_more">view more</a>
     </div>
 </div>
 
