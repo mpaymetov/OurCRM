7e8430e386bc809c45cdec256084fee23471ce31
mpaymetov <muxa147@gmail.com>
2018-08-21 20:11:24 +0300

    исправил форму создания пользователя

--- models/SignupForm.php
+++ models/SignupForm.php
@@ -11,7 +11,7 @@ use yii\base\Model;
 class SignupForm extends Model
 {
 
-    public $username;
+    public $login;
     public $email;
     public $password;
 
@@ -21,10 +21,10 @@ class SignupForm extends Model
     public function rules()
     {
         return [
-            ['username', 'trim'],
-            ['username', 'required'],
-            ['username', 'unique', 'targetClass' => '\app\models\User', 'message' => 'This username has already been taken.'],
-            ['username', 'string', 'min' => 2, 'max' => 255],
+            ['login', 'trim'],
+            ['login', 'required'],
+            ['login', 'unique', 'targetClass' => '\app\models\User', 'message' => 'This login has already been taken.'],
+            ['login', 'string', 'min' => 2, 'max' => 255],
             ['email', 'trim'],
             ['email', 'required'],
             ['email', 'email'],
@@ -48,7 +48,7 @@ class SignupForm extends Model
         }
 
         $user = new User();
-        $user->username = $this->username;
+        $user->login = $this->login;
         $user->email = $this->email;
         $user->setPassword($this->password);
         $user->generateAuthKey();
