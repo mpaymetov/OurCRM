bc04740530c2b904de4a7aabd279067ca7af3607
AlexDor <32705886+AlexDorohoff@users.noreply.github.com>
2018-08-21 17:23:28 +0300

    не сохранялся эвент

--- views/event/_form.php
+++ views/event/_form.php
@@ -22,6 +22,8 @@ use yii\widgets\ActiveForm;
 
     <?= $form->field($model, 'id_link')->textInput(['maxlength' => true]) ?>
 
+    <?= $form->field($model, 'id_user')->textInput(['maxlength' => true]) ?>
+
     <div class="form-group">
         <?= Html::submitButton(Yii::t('app', 'Save'), ['class' => 'btn btn-success']) ?>
     </div>
