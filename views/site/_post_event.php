18f02f7171b0068cfd835335dd8f397e517cbe96
AlexDor <32705886+AlexDorohoff@users.noreply.github.com>
2018-09-04 20:33:50 +0300

    Правка бутафорской кнопки

--- views/site/_post_event.php
+++ views/site/_post_event.php
@@ -8,11 +8,12 @@ use yii\helpers\Url;
 ?>
 <div class="post panel">
     <div class="panel-body">
-        <p class="post_number"><?= \Yii::t('common', 'event number: ')?> <?= HtmlPurifier::process($model->id_event) ?></p>
-        <h4><?= Html::encode($model->message) ?></h4>
         <form class="active_button" action="event.php">
             <p>active <input type="checkbox" value="Отправить"></p>
         </form>
+        <p class="post_number"><?= \Yii::t('common', 'event number: ')?> <?= HtmlPurifier::process($model->id_event) ?></p>
+
+        <h4><?= Html::encode($model->message) ?></h4>
         <a href=<?= Url::toRoute(['event/view', 'id' => $model->id_event]) ?> class="btn btn_more"><?= \Yii::t('common', 'view more')?></a>
     </div>
 </div>
