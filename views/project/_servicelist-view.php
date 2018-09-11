3e8e0bae6eb2f31b8256e61ad07d684ebe7c4251
ZinaidaIvanova <zinaivg@inbox.ru>
2018-09-11 22:52:34 +0300

    Поправила projectView

--- views/project/_servicelist-view.php
+++ views/project/_servicelist-view.php
@@ -5,7 +5,21 @@ use yii\helpers\HtmlPurifier;
 use yii\helpers\Url;
 ?>
 
-<div>
- <?= Html::encode($model['state']);
- ?>
+<div class="serviceset-info">
+ <div>
+  <h3>
+     <?php
+        echo \Yii::t('common', 'serviceset number: ');
+        echo HtmlPurifier::process($model['id']);
+     ?>
+  </h3>
+  <a href='<?= Url::to(['serviceset/update', 'id' => $model['id']]) ?>' class="btn btn-primary pull-right"><?= \Yii::t('common', 'Update')?></a>
+  <p>
+      <?php
+      echo \Yii::t('common', 'State: ');
+      echo Html::encode($model['state']);
+      ?>
+  </p>
+ </div>
 </div>
+
