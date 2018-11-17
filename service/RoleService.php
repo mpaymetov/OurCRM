f0a86c263faf622935463b1122a78f239755be7b
AlexDor <32705886+AlexDorohoff@users.noreply.github.com>
2018-11-17 13:29:43 +0300

    Сервис работы с ролями, метод получения роли

---
+++ service/RoleService.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace app\service;
+
+use app\controllers\SecurityController;
+use Yii;
+use app\models\Event;
+use app\models\EventSearch;
+use app\service\UserServise;
+use app\service\StartParamsService;
+
+
+class RoleService
+{
+
+    public static function getRole()
+    {
+        return Yii::$app->authManager->getRolesByUser(Yii::$app->user->identity->id_user);
+    }
+}
