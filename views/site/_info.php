b3aa39cc741cfb9813a78c480202b9f1fcc0438d
ZinaidaIvanova <zinaivg@inbox.ru>
2018-12-19 19:07:01 +0300

    Поправила site/index

--- views/site/_info.php
+++ views/site/_info.php
@@ -12,22 +12,22 @@ use yii\helpers\Url;
 echo Html::beginTag('div', ['class'=>'serviceset_info post panel']);
 echo Html::beginTag('div', ['class'=>'panel-body']);
 
-$content = \Yii::t('common', 'Client: ') . html::encode($model['client']);
+$content = \Yii::t('common', 'Client') . ': ' . html::encode($model['client']);
 echo Html::tag('p', $content);
 
-$content = \Yii::t('common', 'Project: ') . html::encode($model['project_name']);
+$content = \Yii::t('common', 'Project') . ': ' . html::encode($model['project_name']);
 echo Html::tag('p', $content);
 
 //$content = \Yii::t('common', 'Create date: ') . html::encode($model['create_date']);
 //echo Html::tag('p', $content);
 
-$content = \Yii::t('common', 'Payment date: ') . html::encode($model['payment_date']);
+//$content = \Yii::t('common', 'Payment date') . ': ' . html::encode($model['payment_date']);
+//echo Html::tag('p', $content);
+
+$content = \Yii::t('common', 'Cost') . ': ' . html::encode($model['cost']);
 echo Html::tag('p', $content);
 
-$content = \Yii::t('common', 'Cost: ') . html::encode($model['cost']);
-echo Html::tag('p', $content);
-
-$content = \Yii::t('common', 'Comment: ') . html::encode($model['comment']);
+$content = \Yii::t('common', 'Comment') . ': ' . html::encode($model['comment']);
 echo Html::tag('p', $content);
 
 echo Html::a(\Yii::t('common', 'view more'), Url::to(['project/view', 'id' => $model['id']]));
