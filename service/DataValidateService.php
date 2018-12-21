023bda17844668addf54fa97f9d1a8b9e2c9f6e1
AlexDor <32705886+AlexDorohoff@users.noreply.github.com>
2018-12-21 03:34:34 +0300

    переделал класс валидации

--- service/DataValidateService.php
+++ service/DataValidateService.php
@@ -9,6 +9,7 @@
 namespace app\service;
 
 use app\models\Person;
+use app\models\Client;
 use Yii;
 
 class DataValidateService
@@ -27,85 +28,43 @@ class DataValidateService
 
     public function checkElemAvailable($model)
     {
-        $creator = $model->id_user;
-        if ($creator == Yii::$app->user->identity->id_user || //todo првоерка на доступнось
-            $this->roleService->getRole(Yii::$app->user->identity->id_user >= 2)) {
-            return true;
+
+        if ($model->tableName() == 'person') {
+            if ($this->validateCreatePersonParams($model)) {
+                return true;
+            };
+        }
+        if ($model->tableName() == 'serviceset') {
+            if ($this->validateServisesetParam($model)) {
+                return true;
+            }
+        } else {
+            $creator = $model->id_user;
+            if ($creator == Yii::$app->user->identity->id_user ||
+                $this->roleService->getRole(Yii::$app->user->identity->id_user >= 2)) {
+                return true;
+            }
         }
     }
 
     public function dataControl($model)
     {
         if ($this->checkElemAvailable($model)) {
-            switch ($model->tableName()) {
-                case 'event':
-                    if ($this->validateCreateEventParam($model)) {
-                        return $model;
-                    }
-                    break;
-                case 'project':
-                    if ($this->validateCreateProjectParam($model)) {
-                        return $model;
-                    }
-                    break;
-                case 'client':
-                    if ($this->validateCreateClientParam($model)) {
-                        return true;
-                    }
-                    break;
-                case 'user':
-                    if ($this->validateCreateUserParam($model)) {
-                        return $model;
-                    }
-                    break;
-                case 'person':
-                    if ($this->validateCreatePersonParams($model)) {
-                        return $model;
-                    }
-                    break;
-                default:
-                    return false;
+            if ($this->validateParams($model)) {
+                return $model;
             }
-
         } else {
             return false;
         }
     }
 
-    public function validateCreateEventParam($model)
+    public function validateParams($model)
     {
-        if (!property_exists($model, 'link') && (!property_exists($model, 'id_link'))) {
+        if ($model->validate()) {
             return true;
         } else {
             return false;
         }
-    }
-
-//todo определить область применения, и станет ясно, что проверять
-
-    public function validateCreateProjectParam($model)
-    {
-        if (!property_exists($model, 'id_client')) {
-            return true;
-        } else {
-            return false;
-        }
-    }
-
-    //todo решить необходимость присутствия id клиента в проекте
-
-
-    public function validateCreateClientParam($model)
-    {
-        if (!property_exists($model, 'name') && !property_exists($model, 'comment')) {
-            return $model;
-        }
-        return false;
-    }
-
-    public function validateCreateUserParam($model)
-    {
-        return $model;
     }
 
     public function validateServisesetParam($model)
@@ -117,68 +76,61 @@ class DataValidateService
 
     public function validateCreatePersonParams($model)
     {
-        if (Person::findOne($model->id_user) == Yii::$app->user->identity->id_user) {
+        return true;
+        /*
+        if (Client::findOne($model->id_client)->id_user == Yii::$app->user->identity->id_user) {
             return true;
         }
         return false;
-    }
-/*
-    public function preDataControl($model)
-    {
-        if($this->dataControl($model))
-        {
-            return true;
-        }else{
-            return false;
-        }
+ */
     }
 
-    public function postDataControll($model)
-    {
-        if($this->postValidate($model))
-        {
-            return true;
-        }else{
-            return false;
-        }
-    }
 
-    public function postValidate($model){
-        $model->validate();
-        if ($this->checkElemAvailable($model)) {
-            switch ($model->tableName()) {
-                case 'event':
-                    if ($this->validateCreateEventParam($model)) {
-                        return $model;
-                    }
-                    break;
-                case 'project':
-                    if ($this->validateCreateProjectParam($model)) {
-                        return $model;
-                    }
-                    break;
-                case 'client':
-                    if ($this->validateCreateClientParam($model)) {
-                        return true;
-                    }
-                    break;
-                case 'user':
-                    if ($this->validateCreateUserParam($model)) {
-                        return $model;
-                    }
-                    break;
-                case 'person':
-                    if ($this->validateCreatePersonParams($model)) {
-                        return $model;
-                    }
-                    break;
-                default:
-                    return false;
-            }
 
-        } else {
-            return false;
-        }
-    }
-*/
+    /*  public function dataControl($model)
+      {
+          if ($this->checkElemAvailable($model)) {
+              switch ($model->tableName()) {
+                  case 'event':
+                      if ($this->validateCreateEventParam($model)) {
+                          return $model;
+                      }
+                      break;
+                  case 'project':
+                      if ($this->validateCreateProjectParam($model)) {
+                          return $model;
+                      }
+                      break;
+                  case 'client':
+                      if ($this->validateCreateClientParam($model)) {
+                          return true;
+                      }
+                      break;
+                  case 'user':
+                      if ($this->validateCreateUserParam($model)) {
+                          return $model;
+                      }
+                      break;
+                  case 'person':
+                      if ($this->validateCreatePersonParams($model)) {
+                          return $model;
+                      }
+                      break;
+                  default:
+                      return false;
+              }
+
+          } else {
+              return false;
+          }
+      }
+
+      public function validateCreateClientParam($model)
+      {
+          if (!property_exists($model, 'name') && !property_exists($model, 'comment')) {
+              return $model;
+          }
+          return false;
+      }
+  */
 }
