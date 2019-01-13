2d1f02688f8c04e2a8eb144aa9f510dcb1b0da2e
mpaymetov <muxa147@gmail.com>
2019-01-13 17:53:37 +0300

    change layout for user controller
    настроил страницу авторизации на web-версии, после авторизации приложение переключается на react. админка на web

--- API/controllers/UserController.php
+++ API/controllers/UserController.php
@@ -1,20 +1,252 @@
 <?php
-namespace app\api\controllers;
-use yii\rest\ActiveController;
-use yii\web\Response;
 
-class UserController extends ActiveController
+namespace app\api\controllers;
+
+use Yii;
+use app\service\UserService;
+use app\forms\LoginForm;
+use app\forms\SignupForm;
+use app\forms\ResetForm;
+use app\forms\CreateForm;
+use app\forms\ViewForm;
+use yii\web\Controller;
+use yii\web\NotFoundHttpException;
+use yii\filters\VerbFilter;
+use yii\db\StaleObjectException;
+use app\service\RbacService;
+
+/**
+ * UserController implements the CRUD actions for User model.
+ */
+class UserController extends Controller
 {
-    public $modelClass = 'app\models\User';
-    public function behaviors()
+    public $layout = '@app/views/layouts/user.php';
+
+    private $userService;
+    private $rbacService;
+
+    public function init()
     {
-        $behaviors = parent::behaviors();
-        $behaviors['contentNegotiator']['formats']['text/html'] = Response::FORMAT_JSON;
-        return $behaviors;
+        $this->getService();
     }
 
-    public $serializer = [
-        'class' => 'yii\rest\Serializer',
-        'collectionEnvelope' => 'items',
-    ];
+
+    public function getService()
+    {
+        $this->userService = new UserService();
+        $this->rbacService = new RbacService();
+    }
+
+    public function behaviors()
+    {
+        return [
+            'verbs' => [
+                'class' => VerbFilter::className(),
+                'actions' => [
+                    'delete' => ['POST'],
+                ],
+            ],
+        ];
+    }
+
+    /**
+     * Lists all User models.
+     * @return mixed
+     */
+    public function actionIndex()
+    {
+        return $this->render('index', $this->userService->actionUserIndexRequest());
+    }
+
+    /**
+     * Displays a single User model.
+     * @param string $id
+     * @return mixed
+     * @throws NotFoundHttpException if the model cannot be found
+     */
+    public function actionView($id)
+    {
+        return $this->render('view', [
+            'model' => $this->findModel($id),
+        ]);
+    }
+
+    /**
+     * Disable a single User model.
+     * @param string $id
+     * @return mixed
+     * @throws NotFoundHttpException if the model cannot be found
+     */
+    public function actionDisable($id)
+    {
+        $model = UserService::findModel($id);
+        $model->status = 0;
+        $model->save();
+
+        return $this->render('view', [
+            'model' => $this->findModel($id),
+        ]);
+    }
+
+    /**
+     * Enable a single User model.
+     * @param string $id
+     * @return mixed
+     * @throws NotFoundHttpException if the model cannot be found
+     */
+    public function actionEnable($id)
+    {
+        $model = UserService::findModel($id);
+        $model->status = 10;
+        $model->save();
+
+        return $this->render('view', [
+            'model' => $this->findModel($id),
+        ]);
+    }
+
+    /**
+     * Creates a new User model.
+     * If creation is successful, the browser will be redirected to the 'view' page.
+     * @return mixed
+     */
+    public function actionCreate()
+    {
+        $model = new CreateForm();
+
+        if ($model->load(Yii::$app->request->post())) {
+            if ($user = $model->create()) {
+                $this->redirect(['view', 'id' => $user->id_user]);
+            }
+        }
+
+        $model->roles = RbacService::getRoleList();
+
+        return $this->render('create', [
+            'model' => $model,
+        ]);
+    }
+
+    /**
+     * Updates an existing User model.
+     * If update is successful, the browser will be redirected to the 'view' page.
+     * @param string $id
+     * @return mixed
+     * @throws NotFoundHttpException if the model cannot be found
+     */
+    public function actionUpdate($id)
+    {
+        $user = $this->findModel($id);
+        $model = new CreateForm();
+        try {
+            if ($model->load(Yii::$app->request->post())) {
+                //&& $model->save()
+                return $this->redirect(['view', 'id' => $model->id_user]);
+            }
+
+            $model->login = $user->login;
+            $model->first_name = $user->first_name;
+            $model->second_name = $user->second_name;
+            $model->id_department = $user->id_department;
+            $model->email = $user->email;
+            $model->role = Yii::$app->authManager->getRole($model->login);
+
+            $model->roles = RbacService::getRoleList();
+
+            return $this->render('update', [
+                'model' => $model,
+            ]);
+        } catch (StaleObjectException $e) {
+
+            throw new StaleObjectException(Yii::t('app', 'Error data version'));
+        }
+    }
+
+    public function actionReset($id)
+    {
+        $model = new ResetForm();
+
+        if ($model->load(Yii::$app->request->post())) {
+            if ($user = $model->reset()) {
+                return $this->redirect(['view', 'id' => $user->id_user]);
+            }
+        }
+
+        $model->login = UserService::findLoginById($id);
+
+        return $this->render('reset', [
+            'model' => $model,
+        ]);
+    }
+
+    public function actionLogin()
+    {
+        if (!Yii::$app->user->isGuest) {
+            return $this->goHome();
+        }
+        $model = new LoginForm();
+        if ($model->load(Yii::$app->request->post()) && $model->login()) {
+            return $this->goBack();
+        }
+        $model->password = '';
+        return $this->render('login', [
+            'model' => $model,
+        ]);
+    }
+
+    /**
+     * Logout action.
+     */
+    public function actionLogout()
+    {
+        Yii::$app->user->logout();
+        return $this->goHome();
+    }
+
+    public function actionSignup()
+    {
+        $model = new SignupForm();
+
+        if ($model->load(Yii::$app->request->post())) {
+            if ($user = $model->signup()) {
+                if (Yii::$app->getUser()->login($user)) {
+                    return $this->goHome();
+                }
+            }
+        }
+
+        return $this->render('signup', [
+            'model' => $model,
+        ]);
+    }
+
+    /**
+     * Deletes an existing User model.
+     * If deletion is successful, the browser will be redirected to the 'index' page.
+     * @param string $id
+     * @return mixed
+     * @throws NotFoundHttpException if the model cannot be found
+     */
+    public function actionDelete($id)
+    {
+        $this->findModel($id)->delete();
+
+        return $this->redirect(['index']);
+    }
+
+    /**
+     * Finds the User model based on its primary key value.
+     * If the model is not found, a 404 HTTP exception will be thrown.
+     * @param string $id
+     * @return User the loaded model
+     * @throws NotFoundHttpException if the model cannot be found
+     */
+    protected function findModel($id)
+    {
+        if (($model = UserService::findModel($id)) !== null) {
+            return $model;
+        }
+
+        throw new NotFoundHttpException(Yii::t('app', 'The requested page does not exist.'));
+    }
 }
---
+++ views/layouts/user.php
@@ -0,0 +1,100 @@
+<?php
+
+/* @var $this \yii\web\View */
+
+/* @var $content string */
+
+use app\widgets\Alert;
+use yii\helpers\Html;
+use yii\bootstrap\Nav;
+use yii\bootstrap\NavBar;
+use yii\widgets\Breadcrumbs;
+use app\assets\AppAsset;
+
+AppAsset::register($this);
+?>
+<?php $this->beginPage() ?>
+    <!DOCTYPE html>
+<html lang="<?= Yii::$app->language ?>">
+    <head>
+        <meta charset="<?= Yii::$app->charset ?>">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="/css/site.css">
+        <?php /*<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css">
+        */ ?>
+        <?= Html::csrfMetaTags() ?>
+        <title><?= Html::encode($this->title) ?></title>
+        <?php $this->head() ?>
+    </head>
+    <body>
+    <div class="wrap">
+
+        <?php
+        NavBar::begin([
+            'brandLabel' => Yii::$app->name,
+            'brandUrl' => Yii::$app->homeUrl,
+            'options' => [
+                'class' => 'navbar-inverse navbar-fixed-top',
+            ],
+        ]);
+
+        $menuItems = [
+            ['label' => \Yii::t('common', 'Items'), 'url' => ['/item/index']],
+            ['label' => \Yii::t('common', 'Client'), 'url' => ['/client/index']],
+            ['label' => \Yii::t('common', 'Project'), 'url' => ['/project/index']],
+            ['label' => \Yii::t('common', 'Not complited'), 'url' => ['/events-not-complited/index']],
+            ['label' => \Yii::t('common', 'Department'), 'url' => ['/department/index']],
+            ['label' => \Yii::t('common', 'Service'), 'url' => ['/service/index']],
+            ['label' => \Yii::t('common', 'Event'), 'url' => ['/event/index']],
+            ['label' => \Yii::t('common', 'Statistic'), 'url' => ['/statistic/index']],
+        ];
+
+        if (Yii::$app->user->isGuest) {
+            $menuItems[] = ['label' => \Yii::t('common', 'Log in'), 'url' => ['/user/login']];
+        } else {
+            $menuItems[] = ['label' => \Yii::t('common', 'Users'), 'url' => ['/user/index']];
+            $menuItems[] = '<li>'
+                . Html::beginForm(['/user/logout'], 'post')
+                . Html::submitButton(
+                    \Yii::t('common', 'Logout (') . Yii::$app->user->identity->login . ')',
+                    ['class' => 'btn btn-link logout']
+                )
+                . Html::endForm()
+                . '</li>';
+        }
+
+        echo Nav::widget([
+            'options' => ['class' => 'navbar-nav navbar-right'],
+            'items' => $menuItems
+        ]);
+        NavBar::end();
+        ?>
+
+        <div class="container back_container">
+            <?= Breadcrumbs::widget([
+                'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
+            ]) ?>
+            <?= Alert::widget() ?>
+            <?= $content ?>
+        </div>
+    </div>
+</div>
+
+<footer class="footer">
+    <div class="container">
+        <p class="pull-left">&copy; My Company <?= date('Y') ?> </p>
+        <p class="pull-right"><?= Yii::powered() ?></p>
+
+            <?= Html::beginForm(['/site/language'])?>
+            <?= Html::dropDownList('language', Yii::$app->language, ['en' => 'English', 'ru' => 'Русский']) ?>
+            <?= Html::submitButton('Change') ?>
+            <?= Html::endForm() ?>
+
+    </div>
+</footer>
+
+<?php $this->endBody() ?>
+</body>
+</html>
+<?php $this->endPage() ?>
