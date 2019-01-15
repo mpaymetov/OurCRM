4c0f55fb4d94024cfb9f7a9837877d1f9154cb9e
AlexDor <32705886+AlexDorohoff@users.noreply.github.com>
2019-01-15 03:36:20 +0300

    отображение события

--- react-app/src/component/event/eventView.jsx
+++ react-app/src/component/event/eventView.jsx
@@ -3,15 +3,41 @@ import React, {Component} from 'react';
 class EventView extends Component {
     constructor(props) {
         super(props);
-        this.state = {jsonData: ''};
+        this.state = this.props;
     }
 
     render() {
-        return <div className="done">
-            <p>Match: {JSON.stringify(this.props.match)}</p>
-            <p>Location {JSON.stringify(this.props.location)}</p>
-            <p>Id: {this.props.match.params.id_event}</p>
-        </div>;
+        console.log(this.state);
+        return   <div className="form_wrap panel view_panel">
+            <div className="inner_form">
+                <form onSubmit={this.handleSubmit}>
+                    <p>
+                        <label>Сообщение</label><br/>
+                        <input type="text" className="form-control" value={this.state.message}/>
+                    </p>
+                    <p>
+                        <label>Создано</label><br/>
+                        <input type="text" className="form-control" value={this.state.created}/>
+                    </p>
+                    <p>
+                        <label>Исполнитель</label><br/>
+                        <input type="text" className="form-control" value={this.state.id_doer}/>
+                    </p>
+                    <p>
+                        <label>назначение</label><br/>
+                        <input type='text' className="datepicker-here form-control" data-timepicker="true"
+                               data-position="right top"/>
+                    </p>
+                    <p>
+                        <label>версия</label><br/>
+                        <input type="text" className="form-control" value={this.state.version}/>
+                    </p>
+                    <input type="submit" value="Изменить"/>
+                    <input type="submit" value="Удалить"/>
+
+                </form>
+            </div>
+        </div>
     }
 }
 export default EventView;
